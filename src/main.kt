fb2840c3e090f2413663c9c46c794575a0eb0912
juliett-wanjiru <wanjiruujuliet@gmail.com>
2022-03-10 09:01:20 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var school=("Codehive")
+    var char5=school[4]
+    var char6=school[5]
+    var char7=school[6]
+   var  char8=school[7]
+    var result="$char5$char6$char7$char8"
+    println(result)
+fun letter=make("Judy",19)
+    var string="Wanjiru"
+println("The length of my name is" +string.length)
+
+}
+fun make(name:String,age:Int):String{
+    var cat=("Hi my name is$name and i $age years old")
+return cat
+}
